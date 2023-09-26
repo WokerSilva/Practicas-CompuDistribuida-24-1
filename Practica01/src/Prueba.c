f0ee9a9e613c81fe4122193dee53cf96d3c5837c
carloscortes2000 <Carlos_Daniel_Cortes_Jimenez@ciencias.unam.mx>
2023-09-25 23:26:50 -0600

    se agrearon comentarios

--- Practica01/src/Prueba.c
+++ Practica01/src/Prueba.c
@@ -1,3 +1,9 @@
+//Trabajo realizado por:
+
+//Edgar Montiel Ledesma 
+//Marco Antonio Rivera Silva 
+//Carlos Daniel Cortes Jimenez 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
@@ -6,14 +12,17 @@
 
 #define INFINITY INT_MAX
 
+//Funcion que simula los retrasos tomando números aleatorios entre 1 y 1000
 int randomDelay() {
     return rand() % 1000 + 1;
 }
 
+// Función para encontrar el nodo que no ha visitado con el retraso mínimo
 int minDelay(int delay[], int visited[], int n) {
     int min = INFINITY;
     int minIndex = -1;
 
+    // Busca el nodo con el retraso mínimo que aún no ha sido visitado
     for (int v = 0; v < n; v++) {
         if (!visited[v] && delay[v] < min) {
             min = delay[v];
@@ -24,6 +33,7 @@ int minDelay(int delay[], int visited[], int n) {
     return minIndex;
 }
 
+// Imprime los resultados del algoritmo Dijkstra
 void printDijkstra(int delay[], int parent[], int n) {
     printf("Nodo\tRetraso\tRuta desde 0\n");
     for (int i = 1; i < n; i++) {
@@ -37,11 +47,13 @@ void printDijkstra(int delay[], int parent[], int n) {
     }
 }
 
+// Función principal que realiza el algoritmo de Dijkstra
 void dijkstra(int **graph, int n, int rank, int size) {
     int *delay = (int *)malloc(n * sizeof(int));
     int *parent = (int *)malloc(n * sizeof(int));
     int *visited = (int *)malloc(n * sizeof(int));
 
+    //Se inicializan los arreglos de distancia, padres y visitados
     for (int i = 0; i < n; i++) {
         delay[i] = INFINITY;
         visited[i] = 0;
@@ -50,15 +62,16 @@ void dijkstra(int **graph, int n, int rank, int size) {
 
     delay[0] = 0;
 
+    // Algoritmo de Dijkstra
     for (int count = 0; count < n - 1; count++) {
         int u = minDelay(delay, visited, n);
 
-        // Distribuir el trabajo entre los procesos utilizando OpenMPI
+        // Se distribuye el trabajo entre los procesos utilizando OpenMPI
         int chunk_size = n / size;
         int start = rank * chunk_size;
         int end = (rank == size - 1) ? n : start + chunk_size;
 
-        // Calcular las distancias parciales
+        // Calculamos las distancias parciales
         for (int v = start; v < end; v++) {
             if (!visited[v] && graph[u][v] && delay[u] != INFINITY) {
                 int newDelay = delay[u] + graph[u][v];
@@ -69,17 +82,17 @@ void dijkstra(int **graph, int n, int rank, int size) {
             }
         }
 
-        // Sincronizar los resultados entre los procesos
+        // Sincronizamos los resultados entre los procesos
         MPI_Allreduce(MPI_IN_PLACE, delay, n, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
         MPI_Bcast(parent, n, MPI_INT, 0, MPI_COMM_WORLD);
 
         visited[u] = 1;
     }
-
+    // El proceso con rango 0 imprime los resultados
     if (rank == 0) {
         printDijkstra(delay, parent, n);
     }
-
+    // Liberamos memoria
     free(delay);
     free(parent);
     free(visited);
@@ -100,6 +113,7 @@ int main(int argc, char *argv[]) {
         scanf("%d", &n);
     }
 
+    // Hacemos broadcast del número de nodos a todos los procesos
     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     int **graph = (int **)malloc(n * sizeof(int *));
@@ -107,6 +121,7 @@ int main(int argc, char *argv[]) {
         graph[i] = (int *)malloc(n * sizeof(int));
     }
 
+    // Se Genera el grafo aleatorio en el proceso con rango 0
     if (rank == 0) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -119,8 +134,10 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    // Hacemos broadcast del grafo a todos los procesos
     MPI_Bcast(&(graph[0][0]), n * n, MPI_INT, 0, MPI_COMM_WORLD);
 
+    // Llamamos a la funcion de Dijkstra
     dijkstra(graph, n, rank, size);
 
     for (int i = 0; i < n; i++) {
