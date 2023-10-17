277f06f8cc7c180d6bfd7c25ae87238393b20b4d
WokerSilva <marcosilva@ciencias.unam.mx>
2023-10-16 21:54:45 -0600

    Resolviendo Rebases en testMarco

--- Practica02/src/Practica02_EdgarMontiel_CarlosCortes_MarcoSilva.c
+++ Practica02/src/Practica02_EdgarMontiel_CarlosCortes_MarcoSilva.c
@@ -1,6 +1,11 @@
+//Marco Silva Huerta 316205326
+//Edgar Montiel Ledesma 317317794
+//Carlos Daniel Cortes Jimenez 420004846
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <mpi.h>
 
 #define NUM_GENERALES 10  // Número total de generales
 #define NUM_TRAIDORES 3   // Número de nodos traidores
@@ -13,11 +18,11 @@ typedef struct {
     int es_traidor;   
     int voto;  // 0 para retirada, 1 para ataque
     int mensaje;
-     int estrategia;  // Estrategia de voto de los generales (0 para retirada, 1 para ataque)    
+     int estrategia;  // Estrategia de voto de los generales (0 para retirada, 1 para ataque)  
 } General;
 
 // Función para determinar si la votación es válida
-int esVotacionValida(General generales[]) {
+int esVotacionValida(General generales[], int rank, int size) {
     int votos_ataque = 0;
     int votos_retirada = 0;
 
@@ -35,11 +40,11 @@ int esVotacionValida(General generales[]) {
 
     int mayoria_requerida = (NUM_GENERALES / 2) + F;
 
-    return (votos_ataque >= mayoria_requerida) || (votos_retirada >= mayoria_requerida);
+    return (votos_ataque >= mayoria_requerida || votos_retirada >= mayoria_requerida);
 }
 
 // Función para realizar una ronda de comunicación
-void realizarRonda(General generales[]) {
+void realizarRonda(General generales[], int rank, int size) {
     // Itera a través de cada general en el arreglo generales.
     for (int i = 0; i < NUM_GENERALES; i++) {
         // Elige un voto aleatorio para el general.
@@ -47,9 +52,13 @@ void realizarRonda(General generales[]) {
         // Asigna el voto aleatorio al mensaje del general.
         generales[i].mensaje = generales[i].voto;
     }
+
+    // Comunicación entre generales usando Broadcast
+    MPI_Bcast(generales, NUM_GENERALES * sizeof(General), MPI_BYTE, 0, MPI_COMM_WORLD);
 }
 
-int elegirRey(General generales[]) {
+// Función para elegir un "rey" entre los generales no traidores
+int elegirRey(General generales[], int rank, int size) {
     // Inicialmente, no hay un rey elegido.
     int rey = -1;      
     // Se utiliza para rastrear el ID más alto de los generales no traidores.
@@ -67,18 +76,18 @@ int elegirRey(General generales[]) {
 }
 
 // Función para imprimir el resultado de una ronda y si la votación es válida
-void imprimirResultado(int ronda, General generales[]) {
+void imprimirResultado(int ronda, General generales[], int rank, int size) {
     // Imprime el número de la ronda actual.
     printf("Ronda %d:\n", ronda);
-    
+
     // Itera a través de todos los generales y muestra su ID, si es traidor y su voto.
     for (int i = 0; i < NUM_GENERALES; i++) {
         // Imprime el ID del general y si es traidor o no.
         printf("General %d (Traidor: %s) - Voto: %d\n", generales[i].id, generales[i].es_traidor ? "Sí" : "No", generales[i].voto);
     }
-    
+
     // Verifica si la votación es válida utilizando la función esVotacionValida.
-    if (esVotacionValida(generales)) {
+    if (esVotacionValida(generales, rank, size)) {
         // Si la votación es válida, imprime un mensaje indicando que es válida.
         printf("Votación válida.\n");
     } else {
@@ -89,17 +98,25 @@ void imprimirResultado(int ronda, General generales[]) {
     printf("\n");
 }
 
-int main() {
+int main(int argc, char **argv) {
     srand(time(NULL));
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
     General generales[NUM_GENERALES];
-    
-    // Establecer la estrategia de voto para cada general (puede ajustarse según sea necesario)
-    for (int i = 0; i < NUM_GENERALES; i++) {
-        generales[i].id = i;
-        generales[i].es_traidor = (i < NUM_TRAIDORES) ? 1 : 0;
-        generales[i].voto = -1;  // -1 indica voto indefinido
-        generales[i].mensaje = -1;  // -1 indica mensaje indefinido
-        generales[i].estrategia = rand() % 2;  // Estrategia de voto aleatoria
+
+    if (rank == 0) {
+        // Establecer la estrategia de voto para cada general (puede ajustarse según sea necesario)
+        for (int i = 0; i < NUM_GENERALES; i++) {
+            generales[i].id = i;
+            generales[i].es_traidor = (i < NUM_TRAIDORES) ? 1 : 0;
+            generales[i].voto = -1;  // -1 indica voto indefinido
+            generales[i].mensaje = -1;  // -1 indica mensaje indefinido
+            generales[i].estrategia = rand() % 2;  // Estrategia de voto aleatoria
+        }
     }
 
     int ronda = 0;
@@ -107,27 +124,35 @@ int main() {
 
     while (ronda < MAX_RONDAS) {
         // Realiza una ronda de comunicación entre los generales.
-        realizarRonda(generales);
+        realizarRonda(generales, rank, size);
         // Imprime el resultado de la ronda actual y verifica si la votación es válida.
-        imprimirResultado(ronda, generales);
+        imprimirResultado(ronda, generales, rank, size);
 
         // Si la votación es válida, se rompe el ciclo while.
-        if (esVotacionValida(generales)) {
-            printf("¡Votación válida alcanzada!\n");
+        if (esVotacionValida(generales, rank, size)) {
+            if (rank == 0) {
+                printf("¡Votación válida alcanzada!\n");
+            }
             break;
         } else {
             // Si no se alcanza un consenso, se elige un nuevo "rey" de entre los generales no traidores.
-            rey = elegirRey(generales);
-            printf("No se alcanzó consenso, el General %d es el nuevo rey.\n", rey);
+            rey = elegirRey(generales, rank, size);
+            if (rank == 0) {
+                printf("No se alcanzó consenso, el General %d es el nuevo rey.\n", rey);
+            }
         }
 
-    ronda++;  // Incrementa el número de la ronda.
+        ronda++;  // Incrementa el número de la ronda.
     }
 
     // Si el límite de rondas (MAX_RONDAS) se alcanza sin llegar a un consenso, se muestra un mensaje.
     if (ronda >= MAX_RONDAS) {
-        printf("Se alcanzó el límite de rondas sin consenso.\n");
+        if (rank == 0) {
+            printf("Se alcanzó el límite de rondas sin consenso.\n");
+        }
     }
-   
+
+    MPI_Finalize();
+
     return 0;
 }
