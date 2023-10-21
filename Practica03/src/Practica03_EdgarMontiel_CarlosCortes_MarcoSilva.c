ff2ee025d2abb0b9c13b54c706de620739997d6b
wokersilva <marcosilva@ciencias.unam.mx>
2023-10-21 15:53:41 -0600

    Iniciando Practica03

---
+++ Practica03/src/Practica03_EdgarMontiel_CarlosCortes_MarcoSilva.c
@@ -0,0 +1,6 @@
+// PRACTICA 03 - Computación Distribuida
+//        Algoritmo Abusión
+// Alumnos:
+//  Marco Silva Huerta 316205326
+//  Edgar Montiel Ledesma 317317794
+//  Carlos Daniel Cortes Jimenez 420004846
