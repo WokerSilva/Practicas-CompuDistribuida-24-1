dd5b14e6241232bf9e36a58b9264add24f4985db
WokerSilva <marcosilva@ciencias.unam.mx>
2023-11-21 11:24:11 -0600

    Cambiando forma de imprimir el arrglo

--- Practica04/src/Practica04_EdgarMontiel_CarlosCortes_MarcoSilva.c
+++ Practica04/src/Practica04_EdgarMontiel_CarlosCortes_MarcoSilva.c
@@ -5,16 +5,22 @@
 
 #define ARRAY_SIZE 100
 
+
+// Imprimimos el arrglo en bloques de 15
 void print_array(int arr[], int size) {
     for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]);
+        if ((i + 1) % 15 == 0) {
+            printf("\n");
+        }
     }
     printf("\n");
 }
 
+
 void merge(int arr[], int left[], int left_size, int right[], int right_size) {
     int i = 0, j = 0, k = 0;
-
+    // Compara los elementos de los subarreglos y coloca el menor en el arreglo resultante.
     while (i < left_size && j < right_size) {
         if (left[i] <= right[j]) {
             arr[k++] = left[i++];
@@ -22,11 +28,11 @@ void merge(int arr[], int left[], int left_size, int right[], int right_size) {
             arr[k++] = right[j++];
         }
     }
-
+    // Agrega los elementos restantes del subarreglo izquierdo, si los hay.
     while (i < left_size) {
         arr[k++] = left[i++];
     }
-
+    // Agrega los elementos restantes del subarreglo derecho, si los hay.
     while (j < right_size) {
         arr[k++] = right[j++];
     }
@@ -34,20 +40,28 @@ void merge(int arr[], int left[], int left_size, int right[], int right_size) {
 
 void merge_sort(int arr[], int size) {
     if (size > 1) {
+        // Calcula el punto medio del arreglo.
         int mid = size / 2;
+
+        // Crea subarreglos izquierdo y derecho.
         int left[mid];
         int right[size - mid];
 
+        // Copia elementos al subarreglo izquierdo.
         for (int i = 0; i < mid; i++) {
             left[i] = arr[i];
         }
 
+        // Copia elementos al subarreglo derecho.
         for (int i = mid; i < size; i++) {
             right[i - mid] = arr[i];
         }
 
+        // Llamadas recursivas para ordenar los subarreglos.
         merge_sort(left, mid);
         merge_sort(right, size - mid);
+
+        // Combina los subarreglos ordenados utilizando la función merge.
         merge(arr, left, mid, right, size - mid);
     }
 }
@@ -67,9 +81,11 @@ int main() {
         for (int i = 0; i < ARRAY_SIZE; i++) {
             array[i] = rand() % 1000;
         }
+        
+        printf("\n -- -- -- --    Ordenamiento Distribuido     -- -- -- -- \n");
 
         // 2: Imprimir el arreglo original.
-        printf("Arreglo Original:\n");
+        printf("\n    Arreglo Original:\n\n");
         print_array(array, ARRAY_SIZE);
     }
 
@@ -87,7 +103,7 @@ int main() {
 
     // 6: Imprimir el resultado final.
     if (rank == 0) {
-        printf("Arreglo Ordenado:\n");
+        printf("\n    Arreglo Ordenado:\n\n");
         print_array(array, ARRAY_SIZE);
     }
 
